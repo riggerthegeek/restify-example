a6c671c47c9d2c32c38284f7028ce679ff7d2f06
Simon Emms <simon@simonemms.com>
2014-08-15 22:13:02 +0100

    Added test endpoint

---
+++ src/service/endpoints/TestEndpoint.js
@@ -0,0 +1,27 @@
+/**
+ * Test Endpoint
+ *
+ * The endpoints on the test namespace.
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+
+
+/* Files */
+
+
+module.exports = function () {
+
+
+    return {
+
+    };
+
+
+};
