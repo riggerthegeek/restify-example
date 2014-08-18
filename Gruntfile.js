bae6c4158340f2ca9f4144cf59c46e4b7968dacd
Simon Emms <simon@simonemms.com>
2014-08-18 16:58:36 +0100

    Specified the UI as BDD

--- Gruntfile.js
+++ Gruntfile.js
@@ -116,7 +116,8 @@ module.exports = function (grunt) {
         mochaTest: {
             test: {
                 options: {
-                    reporter: "spec"
+                    reporter: "spec",
+                    ui: "bdd"
                 },
                 src: [
                     "./<%= config.test %>/**/*.js"
