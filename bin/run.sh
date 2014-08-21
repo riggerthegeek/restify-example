436a35b8bc6aea32474a8ef642366fcdcbf191e1
Simon Emms <simon@simonemms.com>
2014-08-21 09:30:31 +0100

    Added Bunyan CLI output

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-steeplejack run --config-file ./config.json --main ./src/Main.js "$@"
+steeplejack run --config-file ./config.json --main ./src/Main.js "$@" | ./node_modules/.bin/bunyan
