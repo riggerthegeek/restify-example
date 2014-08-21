63fc423cb7685c20acba92c1318a006705981e99
Simon Emms <simon@simonemms.com>
2014-08-21 15:39:36 +0100

    Stubbed out the Main file in tests

--- test/Main.test.js
+++ test/Main.test.js
@@ -1,69 +1,112 @@
-///**
-// * Main
-// */
-//
-//"use strict";
-//
-//
-///**
-// * Root Require
-// *
-// * This is a convenience method that prefixes the require
-// * path with the root path so as to help calling the
-// * correct file.
-// *
-// * @param {string} file
-// * @returns {*}
-// */
-//function rootRequire(file) {
-//    return require(require("path").join(process.cwd(), file));
-//}
-//
-//
-///* Node modules */
-//var EventEmitter = require("events").EventEmitter;
-//
-//
-///* Third-party modules */
-//var chai = require("chai");
-//var sinon = require("sinon");
-//
-//var steeplejack = require("steeplejack");
-//
-//
-///* Files */
-//var config = rootRequire("./config");
-//var Main = rootRequire("./src/Main");
-//
-//
-//chai.use(require("sinon-chai"));
-//var expect = chai.expect;
-//
-//
-//describe("Main test", function () {
-//
-//    describe("Instantiation test", function () {
-//
-//        it("should expose a steeplejack instance", function (done) {
-//
-//            var obj = new Main(config);
-//
-//            expect(obj).to.be.instanceof(steeplejack.Base)
-//                .to.be.instanceof(EventEmitter);
-//
-//            obj.on("config", function (serverConfig) {
-//
-//                expect(serverConfig).to.be.an("object");
-//                expect(serverConfig).to.be.equal(config);
-//
-//                done();
-//
-//            });
-//
-//        });
-//
-//    });
-//
-//});
-//
-//
+/**
+* Main
+*/
+
+"use strict";
+
+
+/**
+* Root Require
+*
+* This is a convenience method that prefixes the require
+* path with the root path so as to help calling the
+* correct file.
+*
+* @param {string} file
+* @returns {*}
+*/
+function rootRequire(file) {
+    return require(require("path").join(process.cwd(), file));
+}
+
+
+/* Node modules */
+var EventEmitter = require("events").EventEmitter;
+
+
+/* Third-party modules */
+var chai = require("chai");
+var proxyquire = require("proxyquire");
+var sinon = require("sinon");
+
+var steeplejack = require("steeplejack");
+
+
+/* Files */
+var config = rootRequire("./config");
+
+
+chai.use(require("sinon-chai"));
+var expect = chai.expect;
+
+
+describe("Main test", function () {
+
+    var Main,
+        RestifyInst,
+        RestifyStub;
+    beforeEach(function () {
+
+        RestifyInst = {
+            acceptParserStrict: sinon.stub(),
+            addRoutes: sinon.spy(),
+            bodyParser: sinon.stub(),
+            gzipResponse: sinon.stub(),
+            on: sinon.stub(),
+            outputHandler: sinon.stub(),
+            queryParser: sinon.stub(),
+            start: sinon.stub(),
+            uncaughtException: sinon.stub()
+        };
+
+        RestifyInst.acceptParserStrict
+            .returns(RestifyInst);
+
+        RestifyInst.bodyParser
+            .returns(RestifyInst);
+
+        RestifyInst.gzipResponse
+            .returns(RestifyInst);
+
+        RestifyInst.queryParser
+            .returns(RestifyInst);
+
+        RestifyInst.uncaughtException
+            .returns(RestifyInst);
+
+        RestifyStub = function () {
+            return RestifyInst;
+        };
+
+        Main = proxyquire("../src/Main", {
+            "./service/library/Restify": RestifyStub
+        });
+    });
+
+    describe("Instantiation test", function () {
+
+        it("should expose a steeplejack instance", function (done) {
+
+            RestifyInst.start
+                .yieldsAsync(null);
+
+            var obj = new Main(config)
+                .on("config", function (serverConfig) {
+
+                expect(serverConfig).to.be.an("object");
+                expect(serverConfig).to.be.equal(config);
+
+                done();
+
+            });
+
+            expect(obj).to.be.instanceof(steeplejack.Base)
+                .to.be.instanceof(EventEmitter);
+
+        });
+
+    });
+
+});
+
+
