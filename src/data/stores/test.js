87ec7777eecdb0437f158ea34c79a911ac30da01
Simon Emms <simon@simonemms.com>
2014-08-26 14:24:59 +0100

    Added find by username and password method

--- src/data/stores/test.js
+++ src/data/stores/test.js
@@ -12,6 +12,7 @@
 
 
 /* Third-party modules */
+var _ = require("lodash");
 
 
 /* Files */
@@ -21,6 +22,38 @@ module.exports = function ($logger, $testResource) {
 
 
     return {
+
+        getUserByUsernameAndPassword: function (username, password, cb) {
+
+            $testResource.acquire(function (err, client) {
+
+                if (err) {
+                    return cb(err, null);
+                }
+
+                client.query("SELECT * FROM table", function (err, result) {
+
+                    $testResource.release(client);
+
+                    if (err) {
+                        cb(err, null);
+                        return;
+                    }
+
+                    result = _.where(result, {
+                        username: username,
+                        password: password
+                    });
+
+                    result = result.length === 1 ? result[0] : null;
+
+                    cb(null, result);
+
+                });
+
+            });
+
+        },
 
         getUsers: function (cb) {
 
--- test/unit/data/stores/test.test.js
+++ test/unit/data/stores/test.test.js
@@ -38,26 +38,144 @@ var expect = chai.expect;
 
 describe("Test store test", function () {
 
-    describe("#getUsers", function () {
+    var logger,
+        testRestource,
+        obj,
+        client;
+    beforeEach(function () {
 
-        var logger,
-            testRestource,
-            obj,
-            client;
-        beforeEach(function () {
+        testRestource = {
+            acquire: sinon.stub(),
+            release: sinon.stub()
+        };
 
-            testRestource = {
-                acquire: sinon.stub(),
-                release: sinon.stub()
-            };
+        obj = testStore(logger, testRestource);
 
-            obj = testStore(logger, testRestource);
+        client = {
+            query: sinon.stub()
+        };
 
-            client = {
-                query: sinon.stub()
-            };
+    });
+
+    describe("#getUserByUsernameAndPassword", function () {
+
+        it("should handle an acquire error", function (done) {
+
+            var error = new Error("message");
+            testRestource.acquire
+                .yields(error);
+
+            obj.getUserByUsernameAndPassword("username", "password", function (err, data) {
+
+                expect(err).to.be.equal(error);
+                expect(data).to.be.null;
+
+                expect(testRestource.acquire).to.be.calledOnce;
+                expect(client.query).to.not.be.called;
+
+                done();
+
+            });
 
         });
+
+        it("should handle a query error", function (done) {
+
+            testRestource.acquire
+                .yields(null, client);
+
+            var error = new Error("message");
+
+            client.query
+                .yields(error);
+
+            obj.getUserByUsernameAndPassword("username", "password", function (err, data) {
+
+                expect(err).to.be.equal(error);
+                expect(data).to.be.null;
+
+                expect(testRestource.acquire).to.be.calledOnce;
+                expect(testRestource.release).to.be.calledOnce
+                    .calledWith(client);
+                expect(client.query).to.be.calledOnce
+                    .calledWith("SELECT * FROM table");
+
+                done();
+
+            });
+
+        });
+
+        it("should return a valid query and match the username and password", function (done) {
+
+            testRestource.acquire
+                .yields(null, client);
+
+            var data = [{
+                username: "test1",
+                password: "test"
+            }, {
+                username: "test2",
+                password: "test3"
+            }];
+            client.query
+                .yields(null, data);
+
+            obj.getUserByUsernameAndPassword("test1", "test", function (err, result) {
+
+                expect(err).to.be.null;
+                expect(result).to.be.eql({
+                    username: "test1",
+                    password: "test"
+                });
+
+                expect(testRestource.acquire).to.be.calledOnce;
+                expect(testRestource.release).to.be.calledOnce
+                    .calledWith(client);
+                expect(client.query).to.be.calledOnce
+                    .calledWith("SELECT * FROM table");
+
+                done();
+
+            });
+
+        });
+
+        it("should return a valid query and not match the username and password", function (done) {
+
+            testRestource.acquire
+                .yields(null, client);
+
+            var data = [{
+                username: "test1",
+                password: "test"
+            }, {
+                username: "test2",
+                password: "test3",
+            }];
+            client.query
+                .yields(null, data);
+
+            obj.getUserByUsernameAndPassword("test1", "wrongpassword", function (err, result) {
+
+                expect(err).to.be.null;
+                expect(result).to.be.null;
+
+                expect(testRestource.acquire).to.be.calledOnce;
+                expect(testRestource.release).to.be.calledOnce
+                    .calledWith(client);
+                expect(client.query).to.be.calledOnce
+                    .calledWith("SELECT * FROM table");
+
+                done();
+
+            });
+
+        });
+
+    });
+
+    describe("#getUsers", function () {
 
         it("should handle an acquire error", function (done) {
 
