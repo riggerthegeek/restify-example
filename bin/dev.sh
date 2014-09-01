460f4c258de80d5601786cf4f7dfaf39681198b8
Simon Emms <simon@simonemms.com>
2014-09-01 16:24:12 +0100

    Removed debug flag, which does nothing

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-supervisor -x steeplejack -- run --config-file ./config.json --main ./src/Main.js --debug "$@" | ./node_modules/.bin/bunyan
+supervisor -x steeplejack -- run --config-file ./config.json --main ./src/Main.js "$@" | ./node_modules/.bin/bunyan
