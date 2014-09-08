444a29b04937165dd23ae4bc6ab4290bf23e48d7
Simon Emms <simon@simonemms.com>
2014-09-08 08:40:36 +0100

    More travis investigation

--- test/unit/service/endpoints/TestEndpoint.test.js
+++ test/unit/service/endpoints/TestEndpoint.test.js
@@ -20,6 +20,9 @@ try {
 } catch (err) {
     console.log(err);
     console.log(process.cwd());
+    var fs = require("fs");
+    console.log(fs.readdirSync(process.cwd() + "/src/service/endpoints"));
+    console.log(fs.existsSync(process.cwd() + "/src/service/endpoints/testEndpoint.js"));
     process.exit();
 }
 
