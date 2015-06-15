c71a9f9d24dbda31db94eea99051b1dffd3e2bc7
Simon <simon@simonemms.com>
2015-06-15 20:23:25 +0100

    Added the app and config files

---
+++ src/app.js
@@ -0,0 +1,41 @@
+/**
+ * app
+ */
+
+"use strict";
+
+
+/* Node modules */
+
+
+/* Third-party modules */
+var steeplejack = require("steeplejack");
+var Restify = require("steeplejack-restify");
+
+
+/* Files */
+
+
+var app = steeplejack
+    .app({
+        config: require("./config"),
+        env: require("./envvars"),
+        modules: [
+            "src/!(routes)/**/*.js"
+        ],
+        routes: "routes"
+    })
+    .run(function ($config) {
+
+        return new Restify({
+            port: $config.port
+        });
+
+    })
+    .on("start", function (config) {
+        console.log("Server started on port " + config.port);
+    });
+
+
+/* Export so we can test it */
+module.exports = app;
